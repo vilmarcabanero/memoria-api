f5498155169519e4bfd22cbbfa5588a055d6409d
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-06-13 19:58:38 +0800

    First deploy, done with basic crud, auth nalang

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,10 +1,10 @@
 import Post from '../models/Post.js';
-import chalk from 'chalk';
+import mongoose from 'mongoose';
 
 export const getPosts = async (req, res) => {
 	try {
 		const postMessages = await Post.find();
-		console.log(postMessages);
+		// console.log(postMessages);
 
 		res.send(postMessages);
 	} catch (err) {
@@ -24,5 +24,64 @@ export const createPost = async (req, res) => {
 	} catch (err) {
 		console.log(err);
 		res.status(409).json({ message: err.message });
+	}
+};
+
+export const updatePost = async (req, res) => {
+	try {
+		const { id: _id } = req.params;
+		const post = req.body;
+
+		if (!mongoose.Types.ObjectId.isValid(_id))
+			res.status(404).send('No post with that id');
+
+		const updatedPost = await Post.findByIdAndUpdate(
+			_id,
+			{ ...post, _id },
+			{ new: true }
+		);
+
+		res.json(updatedPost);
+	} catch (err) {
+		console.log(err);
+		res.status(404).json({ message: err.message });
+	}
+};
+
+export const deletePost = async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		if (!mongoose.Types.ObjectId.isValid(id))
+			res.status(404).send('No post with that id');
+
+		await Post.findByIdAndRemove(id);
+
+		res.json({ message: 'Post deleted successfully.' });
+	} catch (err) {
+		console.log(err);
+		res.status(404).json({ message: err.message });
+	}
+};
+
+export const likePost = async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		if (!mongoose.Types.ObjectId.isValid(id))
+			res.status(404).send('No post with that id');
+
+		const post = await Post.findById(id);
+
+		const updatedPost = await Post.findByIdAndUpdate(
+			id,
+			{ likeCount: post.likeCount + 1 },
+			{ new: true }
+		);
+
+		res.json(updatedPost);
+	} catch (err) {
+		console.log(err);
+		res.status(404).json({ message: err.message });
 	}
 };
--- src/routes/post.js
+++ src/routes/post.js
@@ -2,7 +2,10 @@ import express from 'express';
 const router = express.Router();
 import * as p from '../controllers/post.js';
 
-router.get('/', p.getPosts)
-router.post('/', p.createPost)
+router.get('/', p.getPosts);
+router.post('/', p.createPost);
+router.patch('/:id', p.updatePost);
+router.patch('/likePost/:id', p.likePost);
+router.delete('/:id', p.deletePost);
 
 export default router;
